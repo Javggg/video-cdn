4dab426b423c96e407717148de40b6870cd89992
Javggg <jvelasquez@simbiotica.ai>
2022-12-16 01:32:20 -0300

    feat: add models

---
+++ models/Account.model.ts
@@ -0,0 +1,7 @@
+class Account {
+  constructor () {
+    
+  }
+}
+
+export default Account
---
+++ models/Video.model.ts
@@ -0,0 +1,9 @@
+class Video {
+  private name: string
+
+  constructor(name: string) {
+    this.name = name
+  }
+}
+
+export default Video
