a9e63fe5ba94c52ae59b423eeacf061857438d43
Javggg <jvelasquez@simbiotica.ai>
2022-12-17 01:17:51 -0300

    feat: add save method in User model

--- models/User.model.ts
+++ models/User.model.ts
@@ -1,6 +1,27 @@
+import PG from '/config/database.ts'
+
 class User {
-  constructor () {
-    
+  private username: string
+  private password: string
+
+  constructor(username: string, password: string) {
+    this.username = username
+    this.password = password
+  }
+
+  public save = async (): Promise<boolean> => {
+    const query = 'INSERT INTO users (username, password) values ($1, $2)'
+    const args: Array<string> = [this.username, this.password]
+
+    try {
+      await PG.queryObject(query, args)
+      return true
+    } catch (_error) {
+      throw {
+        message: 'Hubo un error al consultar con la base de datos',
+        code: 500
+      }
+    }
   }
 }
 
