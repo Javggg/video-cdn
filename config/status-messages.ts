51a0baf2eb899a5b457385b9be76e2689e8ee537
Javggg <jvelasquez@simbiotica.ai>
2022-12-20 00:37:32 -0300

    docs: add user not found status message

--- config/status-messages.ts
+++ config/status-messages.ts
@@ -1,9 +1,14 @@
 // Types
-import { StatusMessage } from "/types/status-messages.d.ts"
+import { StatusMessage } from '/types/status-messages.d.ts'
 
 const DB_SUCCESSFUL_REQUEST: StatusMessage = {
   message: 'Consulta realizada con exito',
   code: 200
+}
+
+const DB_USER_NOT_FOUND: StatusMessage = {
+  message: 'El usuario introducido no existe',
+  code: 404
 }
 
 const DB_INTERNAL_ERROR: StatusMessage = {
@@ -11,4 +16,4 @@ const DB_INTERNAL_ERROR: StatusMessage = {
   code: 500
 }
 
-export { DB_INTERNAL_ERROR, DB_SUCCESSFUL_REQUEST }
+export { DB_INTERNAL_ERROR, DB_SUCCESSFUL_REQUEST, DB_USER_NOT_FOUND }
--- types/status-messages.d.ts
+++ types/status-messages.d.ts
@@ -1,6 +1,6 @@
 interface StatusMessage {
   message: string
-  code: 200 | 400 | 500
+  code: 200 | 400 | 404 | 500
 }
 
 export { StatusMessage }
