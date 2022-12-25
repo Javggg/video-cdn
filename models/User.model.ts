7f7b5ff1a0592524b54a724c2b1e0b02d118fcdf
Javggg <jvelasquez@simbiotica.ai>
2022-12-25 01:31:30 -0300

    feat: add id property in User class

--- models/User.model.ts
+++ models/User.model.ts
@@ -7,35 +7,38 @@ import PG, { tables } from '/config/database.ts'
 import { config, DotenvConfig } from 'dotenv'
 import {
   DB_INTERNAL_ERROR,
-  DB_SUCCESSFUL_REQUEST,
   LI_USER_OR_PASSWORD_NOT_FOUND,
   SU_USERNAME_ALREADY_EXISTS
 } from '/config/status-messages.ts'
 
 // Types
-import { StatusMessage } from '/types/status-messages.d.ts'
 import { QueryObjectResult } from 'pg-query-types'
 
 const env: DotenvConfig = config()
 
 class User {
   private username: string
+  private id: number | undefined
 
   constructor(username: string) {
     this.username = username
   }
 
-  public signup = async (password: string): Promise<StatusMessage> => {
-    if (await this.userExistsInDb(this.username)) {
+  public signup = async (password: string): Promise<string> => {
+    if (await this.existsInDb(this.username)) {
       throw SU_USERNAME_ALREADY_EXISTS
     }
 
-    const query = `INSERT INTO ${tables.USERS} (username, password) values ($1, $2)`
+    const query = `INSERT INTO ${tables.USERS} (username, password) values ($1, $2) RETURNING id`
     const args: Array<string> = [this.username, await this.hashPassword(password)]
 
     try {
-      await PG.queryObject(query, args)
-      return DB_SUCCESSFUL_REQUEST
+      const { rows }: QueryObjectResult<{ id: number }> = await PG.queryObject(query, args)
+
+      this.id = rows[0].id
+      return await this.generateJWT({
+        id: rows[0].id
+      })
     } catch (_) {
       throw DB_INTERNAL_ERROR
     }
@@ -61,6 +64,8 @@ class User {
         throw LI_USER_OR_PASSWORD_NOT_FOUND
       }
 
+      this.id = rows[0].id
+
       return await this.generateJWT({
         id: rows[0].id
       })
@@ -69,7 +74,15 @@ class User {
     }
   }
 
-  private userExistsInDb = async (username: string): Promise<boolean> => {
+  public getId = (): number => {
+    if (this.id) return this.id
+    throw {
+      message: 'El usuario no tiene ID, registralo en la base de datos para asignarle una',
+      code: 400
+    }
+  }
+
+  private existsInDb = async (username: string): Promise<boolean> => {
     const query = `SELECT EXISTS (SELECT id FROM ${tables.USERS} WHERE username = $1)`
     const args: Array<string> = [username]
 
